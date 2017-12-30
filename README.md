2f2999ea18c88bf439073aad95660f9c6e110cde
chilingirov <vasko988@gmail.com>
2017-12-30 11:24:14 -0800

    Fixed an issue with the timer

--- README.md
+++ README.md
@@ -11,7 +11,21 @@ The app is built in Vanilla JavaScript. The project shows implementation of mode
 - Open index.html file in your browser
 - Enjoy!
 
+## Dependencies
+- The game is built only using HTML,CSS and  Vanilla JavaScript.
+
+## How to play
+- Press Ok to start the game
+- Try to find the 8 pairs cards
+- When you open two cards and if they are not the same the cards close back
+- If the cards match the change their color to green and stay open
+- After you find all the 8 pairs of cards you will see a message on the screen with your time, moves and stars
+- If your moves are under 25 you win 3 stars, if your moves are between 25 and 30 you win 2 stars and if your moves are more than 35 you win just one star
+- After you finish the game press the OK button to start a new one
+
 ## Live Demo
  [Click]( https://chilingirov.github.io/memory-game/.).
+
+
 
 
--- js/app.js
+++ js/app.js
@@ -17,6 +17,7 @@ function shuffle(array) {
 
 //Model - holds the data of the app
 function Model() {
+    timerOn = false;
     this.endGameStatus = false;
     this.time = 0;
     this.matches = 0;
@@ -43,6 +44,7 @@ Model.prototype.checkEndGame = function(selector) {
 };
 //Reset all the variables inside of the model component
 Model.prototype.resetModel = function() {
+    this.timerOn = false;
     this.endGameStatus = false;
     this.time = 0;
     this.matches = 0;
@@ -86,15 +88,13 @@ View.prototype.createCardsHtml = function(array) {
 View.prototype.updateStars = function(moves) {
     for (var i = 0; i < this.stars.length; i++) {
         if (moves > 30) {
-            this.stars[2].className = "fa fa-star-o";
-        } else if (moves > 25) {
             this.stars[1].className = "fa fa-star-o";
-        } else if (moves > 20) {
+        } else if (moves > 25) {
             this.stars[0].className = "fa fa-star-o";
         } else {
-            for (var j = 0; j < this.stars.length; j++) {
-                this.stars[j].className = "fa fa-star";
-            }
+            this.stars[0].className = "fa fa-star";
+            this.stars[1].className = "fa fa-star";
+            this.stars[2].className = "fa fa-star";
         }
     }
 };
@@ -118,6 +118,7 @@ View.prototype.resetCards = function(cards) {
 function Controller() {
     this.model = new Model();
     this.view = new View();
+    var counter;
     var that = this;
     this.checkMatches = function() {
         if (this.model.show.length === 2) {
@@ -127,6 +128,9 @@ function Controller() {
                 console.log("There is a match!");
                 this.model.show = [];
                 that.model.increaseMatches();
+                if (that.model.matches === 8) {
+                    that.resetStopTimer();
+                }
 
             } else {
                 setTimeout(function() {
@@ -143,17 +147,22 @@ function Controller() {
     this.restart = function() {
         this.view.restart.addEventListener("click", function() {
             that.resetGame();
+            that.resetStopTimer();
         });
     };
+    var tt;
     this.startTimer = function() {
-        setInterval(function() {
-            if (that.model.endGameStatus === false) {
+        if (this.model.moves === 0) {
+            tt = setInterval(function() {
                 that.updateTime();
                 console.log("it works");
-            }
-        }, 1000);
-
-
+            }, 1000);
+        }
+    };
+    this.stopTimer = function() {
+        if (this.model.matches === 8) {
+            clearInterval(tt);
+        }
     };
     //Add the time and the stars to the end game modal window, it takes the number of the moves and the time as //parameters
     this.appendStarsToModal = function(selector, moves, time, timeselector, endmoves) {
@@ -186,11 +195,16 @@ function Controller() {
     };
     //Method that reset the game
     this.resetGame = function() {
+        that.resetStopTimer();
         this.model.resetModel();
         this.view.resetCards(this.view.cards);
         this.view.updateMoves(this.model.moves);
         this.view.updateStars(this.model.moves);
         that.displayCards();
+    };
+    this.resetStopTimer = function() {
+        clearInterval(tt);
+        this.view.timer.innerHTML = "00:00:00";
     };
     //The function close the modal window after you win the game and start a new game
     this.closeModalEnd = function() {
@@ -203,7 +217,6 @@ function Controller() {
     this.closeModalStart = function() {
         this.view.startGameButton.addEventListener("click", function() {
             that.view.modalStart.className += " hide";
-            that.startTimer();
         });
     };
     //Call the method that creates the random card's html
@@ -215,6 +228,8 @@ function Controller() {
     this.addClickEvent = function() {
         this.view.deck.addEventListener("click", function(e) {
             e.preventDefault();
+            that.startTimer();
+            that.stopTimer();
             if (e.target.className === "card") {
                 e.target.className = "card open show";
                 that.model.show.push(e.target);
